63bb7f93a04f8a7685b2f1a06fb66c9f6a9d8d66
Luciano <luchomarfil@gmail.com>
2015-11-22 23:15:57 -0300

    Arreglos

--- Chacarera/db/migrate/20151122154115_create_bills.rb
+++
@@ -1,9 +0,0 @@
-class CreateBills < ActiveRecord::Migration
-  def change
-    create_table :bills do |t|
-      t.belongs_to :customer, index: true
-      t.belongs_to :person, index:true
-      t.timestamps null: false
-    end
-  end
-end
