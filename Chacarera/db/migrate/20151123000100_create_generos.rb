c5927310786f9890991c2f1edab33fec728e6d57
Luciano <luchomarfil@gmail.com>
2015-11-26 08:32:48 -0300

    Mejoras varias

--- Chacarera/db/migrate/20151123000100_create_generos.rb
+++
@@ -1,9 +0,0 @@
-class CreateGeneros < ActiveRecord::Migration
-  def change
-    create_table :generos do |t|
-      t.string :nombre
-      t.timestamps null: false
-    end
-    add_index :generos, :nombre
-  end
-end
